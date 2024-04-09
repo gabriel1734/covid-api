b6f280e9e46d60233329c41e135d910efd5ac3c0
gabriel1734 <gabrielfurniel.gc@outlook.com>
2024-04-08 22:23:54 -0300

    [FIX] parse da url

--- Application/core/App.php
+++ Application/core/App.php
@@ -13,11 +13,12 @@ class App {
     {
         $url = $this->parseUrl();
 
-        
 
-        if(empty($url[0])) {
-            $url[0] = $this->controller;
+        if(empty($url[0]) && empty($url[1])) {
+            $url[1] = $this->controller;
         }
+
+        
 
         $this->getControllerFromUrl($url);
 
@@ -36,25 +37,24 @@ class App {
     }
 
     private function parseUrl() {
-        if (isset($_GET['url'])) {
-            return explode('/', filter_var(rtrim($_GET['url'], '/'), FILTER_SANITIZE_URL));
-        }
+        $REQUEST_URI = explode('/', filter_var($_SERVER['REQUEST_URI'], FILTER_SANITIZE_URL));
+        return $REQUEST_URI;
     }
 
     private function getControllerFromUrl($url) {
-        if (file_exists('../Application/controllers/' . $url[0] . '.php')) {
-            $this->controller = $url[0];
-            unset($url[0]);
+        if (file_exists('../Application/controllers/' . $url[1] . '.php')) {
+            $this->controller = $url[1];
+            unset($url[1]);
         } else {
             $this->pageNotFound = true;
         }
     }
 
     private function getMethodFromUrl($url) {
-        if (isset($url[1])) {
-            if (method_exists($this->controller, $url[1])) {
-                $this->method = $url[1];
-                unset($url[1]);
+        if (isset($url[2])) {
+            if (method_exists($this->controller, $url[2])) {
+                $this->method = $url[2];
+                unset($url[2]);
             } else {
                 $this->pageNotFound = true;
             }
