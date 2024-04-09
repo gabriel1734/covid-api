3ef267cd82b01cf5364ab595afb15ddeae1a3d40
gabriel1734 <gabrielfurniel.gc@outlook.com>
2024-04-08 22:23:25 -0300

    [ADD] components adicionados

---
+++ Application/components/HeaderComponent.php
@@ -0,0 +1,19 @@
+<?php
+namespace Application\components;
+
+class HeaderComponent {
+  public function render() {
+    return ' 
+      <header>
+        <nav class="navBar">
+          <h1>COVID-19</h1>
+          <ul>
+            <li><a href="/">Home</a></li>
+            <li><a href="/countries">Countries</a></li>
+            <li><a href="/about">About</a></li>
+          </ul>
+        </nav>
+      </header>
+    ';
+  }
+}
