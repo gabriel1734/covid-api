4ce3cfb414bcc180e5a23228a239d7a9a7fc7158
gabriel1734 <gabrielfurniel.gc@outlook.com>
2024-04-09 23:55:38 -0300

    [MOD] logica de calculo home

--- Application/controllers/Home.php
+++ Application/controllers/Home.php
@@ -10,7 +10,48 @@ class Home extends Controller
         $Brazil = $Api->getCountryWithData('Brazil');
         $Canada = $Api->getCountryWithData('Canada');
         $Australia = $Api->getCountryWithData('Australia');
-        $this->view('home/index', ['Brazil' => $Brazil, 'Canada' => $Canada, 'Australia' => $Australia]);
+        $log = $Api->getLog();
+        
+        $mortos = 0;
+        $casos = 0;
+
+        foreach ($Brazil as $Country) {
+            $mortos += $Country->Mortos;
+            $casos += $Country->Confirmados;
+        }
+
+        $mortos = number_format($mortos, 0, ',', '.');
+        $casos = number_format($casos, 0, ',', '.');
+        
+        $Brazil = ['mortos' => $mortos, 'casos' => $casos];
+
+        $mortos = 0;
+        $casos = 0;
+
+        foreach ($Canada as $Country) {
+            $mortos += $Country->Mortos;
+            $casos += $Country->Confirmados;
+        }
+
+        $mortos = number_format($mortos, 0, ',', '.');
+        $casos = number_format($casos, 0, ',', '.');
+
+        $Canada = ['mortos' => $mortos, 'casos' => $casos];
+
+        $mortos = 0;
+        $casos = 0;
+
+        foreach ($Australia as $Country) {
+            $mortos += $Country->Mortos;
+            $casos += $Country->Confirmados;
+        }
+
+        $mortos = number_format($mortos, 0, ',', '.');
+        $casos = number_format($casos, 0, ',', '.');
+
+        $Australia = ['mortos' => $mortos, 'casos' => $casos];
+
+        $this->view('home/index', ['Brazil' => $Brazil, 'Canada' => $Canada, 'Australia' => $Australia, 'log' => $log->create_time]);
         $this->view('home/index');
     }
 
